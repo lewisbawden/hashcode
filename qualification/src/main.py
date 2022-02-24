0a053562eb7a9c12416fd33881988c557ebb2ef1
Lewis Bawden <lba@trackman.com>
2022-02-24 21:37:51 +0100

    add some simple ideas for the algorithm part

--- qualification/src/main.py
+++ qualification/src/main.py
@@ -3,7 +3,7 @@ import time
 from glob import glob
 
 from parse_input import parse_input_file
-from optimize import optimize, evalutate
+from optimize import optimize
 from write_output import write_output_file, zip_source
 
 
@@ -11,9 +11,8 @@ def run_one_problem(path):
     print()
     t0 = time.time()
 
-    data = parse_input_file(path)
-    out = optimize(data)
-    evalutate(out)
+    peeps, projects = parse_input_file(path)
+    out = optimize(peeps, projects)
     write_output_file(out, os.path.basename(path))
 
     print(f'Total Time: {time.time() - t0}')
