e6b4ce618bde90bb9bf06a9ff012839e0efed0fb
Klara <klaravolckaert@gmail.com>
2022-02-24 21:58:36 +0100

    parse_file doneish

--- qualification/src/parse_input.py
+++ qualification/src/parse_input.py
@@ -9,12 +9,47 @@ def parse_input_file(path):
 
     n_peeps = int(lines[0].split()[0])
     n_projects = int(lines[0].split()[1])
+    skill_types=set()
+    #skill_types.add('skill')
 
-    return peeps, projects
+    ii = 1
+    for ppeeps in range(n_peeps):
+        name = lines[ii].split()[0]
+        n_skills = int(lines[ii].split()[1])
+        thename = Person(name, n_skills)
+
+        for ss in range(n_skills):
+            thisskill=lines[ii+ss+1].split()[0]
+            skill_types.add(f'{thisskill}')
+            thisskilllev = int(lines[ii + ss+1].split()[1])
+            thename.skill[f'{thisskill}']=thisskilllev
+
+        peeps.append(thename)
+        ii+=n_skills+1
+
+    for pprojects in range(n_projects):
+        name = lines[ii].split()[0]
+        days = int(lines[ii].split()[1])
+        score = int(lines[ii].split()[2])
+        bbd = int(lines[ii].split()[3])
+        n_roles = int(lines[ii].split()[4])
+        thename = Project(name, days, score, bbd, n_roles)
+
+        for ss in range(n_roles):
+            thisskill=lines[ii+ss+1].split()[0]
+            thisskilllev = int(lines[ii + ss+1].split()[1])
+            thename.skill[f'{thisskill}']=thisskilllev
+
+        projects.append(thename)
+        ii+=n_roles+1
+
+
+    return peeps, projects, skill_types
 
 
 class Person:
-    def __init__(self, n_skills):
+    def __init__(self, nname, n_skills):
+        self.nname = nname
         self.n_skills = n_skills
         self.skill = {}
 
@@ -23,13 +58,14 @@ class Person:
 
 
 class Project:
-    def __init__(self, days, score, bbd, roles):
+    def __init__(self, nname, days, score, bbd, roles):
+        self.nname = nname
         self.days = days
         self.score = score
         self.bbd = bbd
         self.roles = roles
 
-        self.skills = {}
+        self.skill = {}
 
     def __repr__(self):
         return f'{self.__dict__}'
